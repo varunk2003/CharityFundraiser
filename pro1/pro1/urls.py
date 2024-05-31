4cd1b3ea241941268bd41970c570fd351ee65084
varunk2003 <141061920+varunk2003@users.noreply.github.com>
2024-05-31 16:01:48 +0530

    Update urls.py

--- pro1/pro1/urls.py
+++ pro1/pro1/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("fundraiser/",include('fundraiser.urls')),
+    path("",include('fundraiser.urls')),
     path('admin/', admin.site.urls),
 ]
